9b0a69fe61ae5c2f4ea4d1bad8822e7eeb5c4009
DaX0d <daniil.khodyrev@gmail.com>
2025-03-25 14:00:56 +0300

    Мини исправление

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
-DATABASE_URL = 'postgresql://postgres:abobus@db:5432/example'
+DATABASE_URL = 'postgresql://postgres:@db:5432/example'
 
 engine = create_engine(DATABASE_URL)
 
